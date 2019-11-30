4523517c4eb1740681e98e54ed57226c92520f83
liangwei <liangwei@itcast.cn>
2019-11-30 16:06:59 +0800

    删除脚手架默认内容,添加顶部公共组件

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
+    <Header />
+    <!-- 内容占位组件 -->
     <nuxt />
   </div>
 </template>
 
-<style>
+<script>
+// 导入头部组件
+import Header from "@/components/header";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="less">
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -17,39 +29,23 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
+* {
   margin: 0;
+  padding: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
   text-decoration: none;
-  padding: 10px 30px;
+  color: inherit;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+a:hover {
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,
+i {
+  font-style: normal;
 }
 </style>
