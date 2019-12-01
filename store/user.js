496dcc82eff149e67e4a6973ea9a0428c4a19fdf
liangwei <liangwei@itcast.cn>
2019-12-01 15:47:31 +0800

    修改vuex user 的 mutation 函数

--- store/user.js
+++ store/user.js
@@ -1,7 +1,20 @@
 // 固定的写法,所有仓库里面的数据都叫 state
+// 这个是用来声明数据的
 export const state = () => ({
     userInfo: {
-        token: '',
+        token: '123',
         user: {}
     }
 })
+
+// 用来改变数据状态的
+export const mutations = {
+    // 每一个属性都是一个函数
+    // 有两个参数,
+    // 第一个,使我们的状态对象 state
+    // 第二个就是我们想要改变的数据
+    setUserInfo(state, data) {
+        // 这里是负责修改用户数据的
+        state.userInfo = data
+    }
+}
