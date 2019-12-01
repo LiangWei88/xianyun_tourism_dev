b90dba6724ed85af3d9be554a680a99dbfc0bab9
liangwei <liangwei@itcast.cn>
2019-12-01 10:50:43 +0800

    登录表单布局和验证

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -1,10 +1,69 @@
 <template>
-  <div>123</div>
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名/手机" v-model="form.username"></el-input>
+    </el-form-item>
+
+    <el-form-item class="form-item" prop="password">
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
+    </el-form-item>
+
+    <p class="form-text">
+      <nuxt-link to="#">忘记密码</nuxt-link>
+    </p>
+
+    <!-- <el-button class="submit" type="primary" @click="handleLoginSubmit">登录</el-button> -->
+    <el-button class="submit" type="primary">登录</el-button>
+  </el-form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
 </style>
