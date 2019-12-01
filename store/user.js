310cb8a05f83b4cf85e051baa6655e587be5a168
liangwei <liangwei@itcast.cn>
2019-12-01 16:21:23 +0800

    vuex 创建登录 action

--- store/user.js
+++ store/user.js
@@ -18,3 +18,22 @@ export const mutations = {
         state.userInfo = data
     }
 }
+
+// mutation 只能执行同步代码,对于异步代码比如获取 ajax 请求数据等,需要使用 actions
+export const actions = {
+    // 里面每一个属性都是函数
+    // 两个参数,第一个是 {commit} 将这个仓库对象本身的 commit 函数解构出来
+    // 第二个参数就是我们需要传进来的 数据
+    login({ commit }, data) {
+        return this.$axios({
+            url: "/accounts/login",
+            method: "POST",
+            data: data
+        }).then(res => {
+            const data = res.data;
+            // 保存到state
+            commit("setUserInfo", data);
+            return data;
+        })
+    }
+}
