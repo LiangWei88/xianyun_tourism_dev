55213a5b1378e652263b2f551638aca818e95acb
liangwei <liangwei@itcast.cn>
2019-12-03 17:07:00 +0800

    获取搜索建议

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -19,6 +19,7 @@
         <!-- select 点击选中建议项时触发 -->
         <el-autocomplete
           :fetch-suggestions="queryDepartSearch"
+          v-model="form.departCity"
           placeholder="请搜索出发城市"
           @select="handleDepartSelect"
           class="el-autocomplete"
@@ -28,6 +29,7 @@
       <el-form-item label="到达城市">
         <el-autocomplete
           :fetch-suggestions="queryDestSearch"
+          v-model="form.destCity"
           placeholder="请搜索到达城市"
           @select="handleDestSelect"
           class="el-autocomplete"
@@ -36,7 +38,13 @@
 
       <el-form-item label="出发时间">
         <!-- change 用户确认选择日期时触发 -->
-        <el-date-picker type="date" placeholder="请选择日期" style="width: 100%;" @change="handleDate"></el-date-picker>
+        <el-date-picker
+          v-model="form.departDate"
+          type="date"
+          placeholder="请选择日期"
+          style="width: 100%;"
+          @change="handleDate"
+        ></el-date-picker>
       </el-form-item>
 
       <el-form-item label>
@@ -58,7 +66,14 @@ export default {
         { icon: "iconfont icondancheng", name: "单程" },
         { icon: "iconfont iconshuangxiang", name: "往返" }
       ],
-      currentTab: 0
+      currentTab: 0,
+      form: {
+        departCity: "", // 出发城市
+        departCode: "", // 出发城市代码
+        destCity: "", // 到达城市
+        destCode: "", // 到达城市代码
+        departDate: "" // 日期字符串
+      }
     };
   },
   methods: {
@@ -67,18 +82,76 @@ export default {
 
     // 出发城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
-    queryDepartSearch(value, cb) {
-      cb([{ value: 1 }, { value: 2 }, { value: 3 }]);
+    // 这个列表其实是一个数组,数组当中每一个元素都是一个对象
+    // 一个对象就是一条建议
+    // 其中最重要的是 value 这个值
+    // queryDepartSearch(value, cb) {
+    // 最简单的建议结构如下
+    // var arr = [
+    //   {
+    //     value: "广州"
+    //   },
+    //   {
+    //     value: "上海"
+    //   }
+    // ];
+    // cb(arr);
+    // },
+
+    async queryDepartSearch(value, cb) {
+      const arr = await this.querySearchAsync(value);
+      if (arr.length > 0) {
+        // 不在下拉列表中选择，则默认选择第一项
+        this.form.departCity = arr[0].value;
+        this.form.departCode = arr[0].sort;
+      }
+      console.log(arr);
+
+      cb(arr);
     },
 
     // 目标城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
-    queryDestSearch(value, cb) {
-      cb([{ value: 1 }, { value: 2 }, { value: 3 }]);
+    async queryDestSearch(value, cb) {
+      const arr = await this.querySearchAsync(value);
+      if (arr.length > 0) {
+        // 不在下拉列表中选择，则默认选择第一项
+        this.form.destCity = arr[0].value;
+        this.form.destCode = arr[0].sort;
+      }
+      cb(arr);
     },
 
     // 出发城市下拉选择时触发
     handleDepartSelect(item) {},
+
+    // 这个是封装的 ajax 专门用来获取城市列表
+    querySearchAsync(queryString) {
+      return new Promise((resolve, reject) => {
+        // 如果关键字是空，则直接返回
+        if (!queryString) {
+          return resolve([]);
+        }
+
+        this.$axios({
+          url: `/airs/city`,
+          params: {
+            name: queryString
+          }
+        }).then(res => {
+          const { data } = res.data;
+
+          // 下拉提示列表必须要有value字段
+          const arr = data.map(v => {
+            return {
+              ...v,
+              value: v.name.replace("市", "")
+            };
+          });
+          resolve(arr);
+        });
+      });
+    },
 
     // 目标城市下拉选择时触发
     handleDestSelect(item) {},
@@ -90,7 +163,9 @@ export default {
     handleReverse() {},
 
     // 提交表单是触发
-    handleSubmit() {}
+    handleSubmit() {
+      console.log(this.form);
+    }
   },
   mounted() {}
 };
