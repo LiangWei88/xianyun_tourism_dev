d58dae611c8fb30df6b6c48504d66446673658e1
liangwei <liangwei@itcast.cn>
2019-12-03 14:47:50 +0800

    表单验证

--- components/user/register.vue
+++ components/user/register.vue
@@ -1,10 +1,10 @@
 <template>
   <el-form :model="form" ref="form" :rules="rules" class="form">
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="username">
       <el-input placeholder="用户名手机" v-model="form.username"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="captcha">
       <el-input placeholder="验证码" v-model="form.captcha">
         <template slot="append">
           <el-button @click="handleSendCaptcha">发送验证码</el-button>
@@ -12,15 +12,15 @@
       </el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="nickname">
       <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="password">
       <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="checkPassword">
       <el-input placeholder="确认密码" type="password" v-model="form.checkPassword"></el-input>
     </el-form-item>
 
@@ -32,11 +32,19 @@
 export default {
   data() {
     const validatePass = (rule, value, callback) => {
+      // 如果我们要跑出一个自定义的错误;
+      // throw Error("这里是一个错误");
+      // 这里面的参数都是固定的
+      // rule 就是传进来的规则对象
+      // value 就是输入框的值
+      // callback 就是校验完之后需要调用的回调函数
       if (value === "") {
+        // 如果出错了,调用回调时,传入一个 error 对象
         callback(new Error("请再次输入密码"));
       } else if (value !== this.form.password) {
         callback(new Error("两次输入密码不一致!"));
       } else {
+        // 如果成功,直接回调
         callback();
       }
     };
