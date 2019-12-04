784de09e656a1a30e23ed51de93885798afa334a
liangwei <liangwei@itcast.cn>
2019-12-04 16:58:22 +0800

    获取航班数据

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -28,11 +28,31 @@ import FlightsListHead from "@/components/air/flightsListHead.vue";
 import FlightsItem from "@/components/air/flightsItem.vue";
 
 export default {
-  // 其他代码...
+  data() {
+    return {
+      flightsData: {}, // 航班总数据
+      dataList: [] // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+    };
+  },
 
   components: {
     FlightsListHead,
     FlightsItem
+  },
+  mounted() {
+    // console.log(this.$route.query);
+    // 对于路由,两种参数分别是 params 和 query
+    // 对于 axios 两种参数分别是 data 和 params
+
+    this.$axios({
+      url: "/airs",
+      //参数可以通过 this.$route.query
+      // 这里面数据本来就是一个对象,所以无需自己拼接
+      params: this.$route.query
+    }).then(res => {
+      this.flightsData = res.data;
+      this.dataList = this.flightsData.flights;
+    });
   }
 };
 </script>
