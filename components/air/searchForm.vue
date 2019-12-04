bbbadc29663548ea42d2dc2ee6857f4d1827e55c
liangwei <liangwei@itcast.cn>
2019-12-04 09:38:58 +0800

    原始搜索框结构

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -2,59 +2,11 @@
   <div class="search-form">
     <!-- 头部tab切换 -->
     <el-row type="flex" class="search-tab">
-      <span
-        v-for="(item, index) in tabs"
-        :key="index"
-        @click="handleSearchTab(item, index)"
-        :class="{active: index === currentTab}"
-      >
+      <span v-for="(item, index) in tabs" :key="index" :class="{active: index === currentTab}">
         <i :class="item.icon"></i>
         {{item.name}}
       </span>
     </el-row>
-
-    <el-form class="search-form-content" ref="form" label-width="80px">
-      <el-form-item label="出发城市">
-        <!-- fetch-suggestions 返回输入建议的方法 -->
-        <!-- select 点击选中建议项时触发 -->
-        <el-autocomplete
-          :fetch-suggestions="queryDepartSearch"
-          v-model="form.departCity"
-          placeholder="请搜索出发城市"
-          @select="handleDepartSelect"
-          class="el-autocomplete"
-        ></el-autocomplete>
-      </el-form-item>
-
-      <el-form-item label="到达城市">
-        <el-autocomplete
-          :fetch-suggestions="queryDestSearch"
-          v-model="form.destCity"
-          placeholder="请搜索到达城市"
-          @select="handleDestSelect"
-          class="el-autocomplete"
-        ></el-autocomplete>
-      </el-form-item>
-
-      <el-form-item label="出发时间">
-        <!-- change 用户确认选择日期时触发 -->
-        <el-date-picker
-          v-model="form.departDate"
-          type="date"
-          placeholder="请选择日期"
-          style="width: 100%;"
-          @change="handleDate"
-        ></el-date-picker>
-      </el-form-item>
-
-      <el-form-item label>
-        <el-button style="width:100%;" type="primary" icon="el-icon-search" @click="handleSubmit">搜索</el-button>
-      </el-form-item>
-
-      <div class="reverse">
-        <span @click="handleReverse">换</span>
-      </div>
-    </el-form>
   </div>
 </template>
 
@@ -66,108 +18,10 @@ export default {
         { icon: "iconfont icondancheng", name: "单程" },
         { icon: "iconfont iconshuangxiang", name: "往返" }
       ],
-      currentTab: 0,
-      form: {
-        departCity: "", // 出发城市
-        departCode: "", // 出发城市代码
-        destCity: "", // 到达城市
-        destCode: "", // 到达城市代码
-        departDate: "" // 日期字符串
-      }
+      currentTab: 0
     };
   },
-  methods: {
-    // tab切换时触发
-    handleSearchTab(item, index) {},
-
-    // 出发城市输入框获得焦点时触发
-    // value 是选中的值，cb是回调函数，接收要展示的列表
-    // 这个列表其实是一个数组,数组当中每一个元素都是一个对象
-    // 一个对象就是一条建议
-    // 其中最重要的是 value 这个值
-    // queryDepartSearch(value, cb) {
-    // 最简单的建议结构如下
-    // var arr = [
-    //   {
-    //     value: "广州"
-    //   },
-    //   {
-    //     value: "上海"
-    //   }
-    // ];
-    // cb(arr);
-    // },
-
-    async queryDepartSearch(value, cb) {
-      const arr = await this.querySearchAsync(value);
-      if (arr.length > 0) {
-        // 不在下拉列表中选择，则默认选择第一项
-        this.form.departCity = arr[0].value;
-        this.form.departCode = arr[0].sort;
-      }
-      console.log(arr);
-
-      cb(arr);
-    },
-
-    // 目标城市输入框获得焦点时触发
-    // value 是选中的值，cb是回调函数，接收要展示的列表
-    async queryDestSearch(value, cb) {
-      const arr = await this.querySearchAsync(value);
-      if (arr.length > 0) {
-        // 不在下拉列表中选择，则默认选择第一项
-        this.form.destCity = arr[0].value;
-        this.form.destCode = arr[0].sort;
-      }
-      cb(arr);
-    },
-
-    // 出发城市下拉选择时触发
-    handleDepartSelect(item) {},
-
-    // 这个是封装的 ajax 专门用来获取城市列表
-    querySearchAsync(queryString) {
-      return new Promise((resolve, reject) => {
-        // 如果关键字是空，则直接返回
-        if (!queryString) {
-          return resolve([]);
-        }
-
-        this.$axios({
-          url: `/airs/city`,
-          params: {
-            name: queryString
-          }
-        }).then(res => {
-          const { data } = res.data;
-
-          // 下拉提示列表必须要有value字段
-          const arr = data.map(v => {
-            return {
-              ...v,
-              value: v.name.replace("市", "")
-            };
-          });
-          resolve(arr);
-        });
-      });
-    },
-
-    // 目标城市下拉选择时触发
-    handleDestSelect(item) {},
-
-    // 确认选择日期时触发
-    handleDate(value) {},
-
-    // 触发和目标城市切换时触发
-    handleReverse() {},
-
-    // 提交表单是触发
-    handleSubmit() {
-      console.log(this.form);
-    }
-  },
-  mounted() {}
+  methods: {}
 };
 </script>
 
