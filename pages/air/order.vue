f7c61e18213ec38b9230b2d63554cc6ce8270e98
liangwei <liangwei@itcast.cn>
2019-12-07 10:47:53 +0800

    订单页基本大布局

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>这里是订单页面</div>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <div class="main">这里放主要内容</div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">这里放侧边栏</div>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
 </style>
