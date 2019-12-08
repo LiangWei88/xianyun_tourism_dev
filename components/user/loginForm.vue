5c9821b5cd4ba0528b34b2b84e84a13eb27bb9e7
liangwei <liangwei@itcast.cn>
2019-12-08 16:44:30 +0800

    登录组件添加可以不跳转的选项

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -18,6 +18,7 @@
 
 <script>
 export default {
+  props: ["disableRedirect"],
   data() {
     return {
       form: {
@@ -59,16 +60,24 @@ export default {
           // });
           // 使用 vuex 的 actions 替换这里直接写的ajax请求
           this.$store.dispatch("user/login", this.form).then(res => {
-            // 成功提示
-            this.$message({
-              message: "登录成功，正在跳转",
-              type: "success"
-            });
-            // 跳转到首页
-            setTimeout(() => {
+            if (!this.disableRedirect) {
+              // 成功提示
+              this.$message({
+                message: "登录成功，正在跳转",
+                type: "success"
+              });
+              // 跳转到首页
+              setTimeout(() => {
+                this.$store.commit("user/setUserInfo", res);
+                this.$router.replace("/");
+              }, 1000);
+            } else {
               this.$store.commit("user/setUserInfo", res);
-              this.$router.replace("/");
-            }, 1000);
+              this.$message({
+                message: "登录成功",
+                type: "success"
+              });
+            }
           });
         }
       });
