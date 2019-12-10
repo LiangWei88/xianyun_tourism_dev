2df2394ef1eb9542bcb0a58268754be3ec7bb777
liangwei <liangwei@itcast.cn>
2019-12-10 15:16:07 +0800

    递归组件

---
+++ components/menuItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in children" :key="index">
+      {{item.name}}
+      <!-- 如果这个item 有 children 就继续把自己这个结构往里传 -->
+      <!-- 开始写递归嵌套 -->
+      <MenuItem v-if="item.children" :children="item.children" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  // 如果想自己调用自己必须医用 name 属性
+  name: "MenuItem",
+  props: ["children"]
+};
+</script>
+
+<style>
+</style>
---
+++ pages/menu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <MenuItem :children="menu" />
+  </div>
+</template>
+
+<script>
+import MenuItem from "@/components/menuItem.vue";
+export default {
+  components: {
+    MenuItem
+  },
+  data() {
+    return {
+      menu: [
+        // 菜单应该是一个数组
+        // 数组里面每一项都是一个对象,对应有一个菜单
+        {
+          name: "男装",
+          children: [
+            // 这里是下级菜单,它的结构,应该跟上级菜单一样
+            {
+              name: "鞋子",
+              children: [
+                //   第三级菜单,应该结构跟第二级第一级是一样的
+                {
+                  name: "AJ 1"
+                },
+                {
+                  name: "AJ X"
+                }
+              ]
+            },
+            {
+              name: "帽子"
+            }
+          ]
+        },
+        {
+          name: "女装"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
