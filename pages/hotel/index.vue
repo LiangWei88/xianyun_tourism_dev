6d29ccb76ffd414bd917e1fe473f77a52004209a
liangwei <liangwei@itcast.cn>
2019-12-10 16:51:47 +0800

    18-工具栏插件使用

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -16,6 +16,7 @@ export default {
         viewMode: "3D" //使用3D视图
       });
 
+      // 点标记*************************
       var marker = new AMap.Marker({
         position: [113.32459, 23.10668], //位置
         // offset: new AMap.Pixel(-10, -10),
@@ -25,6 +26,13 @@ export default {
           "<div style='width:100px;height:100px;line-height:100px;text-align:center;border-radius:50%;color:#fff;background:red;'>999</div>"
       });
       map.add(marker); //添加到地图
+
+      // 插件添加*************************
+      AMap.plugin("AMap.ToolBar", function() {
+        //异步加载插件
+        var toolbar = new AMap.ToolBar();
+        map.addControl(toolbar);
+      });
     };
     var url =
       "https://webapi.amap.com/maps?v=1.4.15&key=ccd10c501bd6a0ca679f86302d98bb85&callback=onLoad";
