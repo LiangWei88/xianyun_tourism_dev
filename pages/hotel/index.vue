3175c49b1778b2b39cfc2d677f2d59d02be13109
liangwei <liangwei@itcast.cn>
2019-12-10 16:25:04 +0800

    16-小蛮腰定位

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>酒店的页面</div>
+  <div style="padding:20px;">
+    <h2>高德地图实例</h2>
+    <div id="map"></div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    window.onLoad = function() {
+      var map = new AMap.Map("map", {
+        zoom: 30, //级别
+        // 东经北纬
+        center: [113.32459, 23.10668], //中心点坐标
+        viewMode: "3D" //使用3D视图
+      });
+    };
+    var url =
+      "https://webapi.amap.com/maps?v=1.4.15&key=ccd10c501bd6a0ca679f86302d98bb85&callback=onLoad";
+
+    var jsapi = document.createElement("script");
+
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+#map {
+  width: 480px;
+  height: 360px;
+}
 </style>
