bac537ccfdaeba6489f29ed6d1fa56964885495b
liangwei <liangwei@itcast.cn>
2019-12-10 10:49:04 +0800

    插槽测试页面, 默认插槽

---
+++ components/slotComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>这里是子组件内容</h2>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/slot.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    这里是父组件: 插槽都是使用在组件当中
+    <SlotComponent>直接在这里输入子组件内容</SlotComponent>
+  </div>
+</template>
+
+<script>
+import SlotComponent from "@/components/slotComponent.vue";
+
+export default {
+  components: {
+    SlotComponent
+  }
+};
+</script>
+
+<style>
+</style>
