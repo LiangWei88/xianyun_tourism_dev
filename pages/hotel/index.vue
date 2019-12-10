b3e8a88a56d8d99a9177fa3c463950f507799071
liangwei <liangwei@itcast.cn>
2019-12-10 16:47:26 +0800

    点标记使用

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -15,6 +15,16 @@ export default {
         center: [113.32459, 23.10668], //中心点坐标
         viewMode: "3D" //使用3D视图
       });
+
+      var marker = new AMap.Marker({
+        position: [113.32459, 23.10668], //位置
+        // offset: new AMap.Pixel(-10, -10),
+        // icon: "//vdata.amap.com/icons/b18/1/2.png", // 添加 Icon 图标 URL
+        title: "广州塔东方赛达森",
+        content:
+          "<div style='width:100px;height:100px;line-height:100px;text-align:center;border-radius:50%;color:#fff;background:red;'>999</div>"
+      });
+      map.add(marker); //添加到地图
     };
     var url =
       "https://webapi.amap.com/maps?v=1.4.15&key=ccd10c501bd6a0ca679f86302d98bb85&callback=onLoad";
